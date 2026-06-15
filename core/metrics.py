f55afb3b72a3df77ccbadbcdd9c1db543eb84f08
swiftswift-swift <mdyaqoob19nov@gmail.com>
2026-06-15 17:20:27 +0530

    Update metrics.py

--- core/metrics.py
+++ core/metrics.py
@@ -1,12 +1,14 @@
 import json
 import time
+import os
 import redis
 
 
 class MetricsCollector:
     def __init__(self):
-        self.redis_client = redis.Redis(
-            host="localhost", port=6379, decode_responses=True
+        self.redis_client = redis.from_url(
+            os.getenv("REDIS_URL"),
+            decode_responses=True
         )
 
     def store_request(self, data: dict):
